fc001d6a3943c94526189b21508989eed8e6c16d
merrypang <40026702+merrypang@users.noreply.github.com>
2018-06-07 19:46:22 +0800

    Create Bootshrap.js

---
+++ Bootshrap.js
@@ -0,0 +1,392 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"> 
+	<title>WEB前端</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css/set2.css" />
+		<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.2.0/css/font-awesome.min.css" />
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" /><!--CSS RESET-->
+	<link rel="stylesheet" type="text/css" href="css/htmleaf-demo.css"><!--演示页面样式，使用时可以不引用-->
+	<link href="css/jCircle.css" rel="stylesheet">
+
+	
+  		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<style>
+		body{
+			background-image:url(img/6855b520f2176ec.jpg);
+			background-size: 100%;
+		}
+		.container{
+			width: 800px;
+			margin: 30px auto; 
+		}
+
+	</style>
+</head>
+<body >
+
+<nav id="navbar-example" class="navbar navbar-default navbar-static" role="navigation" style="background-color: #fff8dc;">
+	<div class="container-fluid"> 
+		<div class="navbar-header">
+			<button class="navbar-toggle" type="button" data-toggle="collapse" 
+					data-target=".bs-js-navbar-scrollspy">
+				<span class="sr-only">切换导航</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#"  style="font-size: 1.5em;">WEB前端</a>
+		</div>
+		<div class="collapse navbar-collapse bs-js-navbar-scrollspy">
+			<ul class="nav navbar-nav">
+				<li><a href="#学习心路">学习心路</a></li>
+				<li><a href="#学习方法">学习方法</a></li>
+				<li><a href="#学习成果">学习成果</a></li>
+			</ul>
+		</div>
+	</div> 
+</nav>
+<div class="container">
+		<div style="width:80%;margin:200px auto;">
+		      <div id="circles-container"> 
+		              <div id="main-circle-content"></div>
+		              <div id="circle">
+		                <div class="min-circle" data-inside="min-circle">
+		                  <a href="img/1.jpg">
+		                    <img src="img/1.jpg" alt="">
+		                  </a>
+		                  <div class="content-text">超帅的WEB前端娃</div>
+		                </div>
+		                <div class="min-circle" data-inside="min-circle">
+		                  <a href="img/10.jpg">
+		                    <img src="img/10.jpg" alt="">
+		                  </a>
+		                  <div class="content-text"></div>
+		                </div>
+		                <div class="min-circle" data-inside="min-circle">
+		                  <a href="img/11.jpg">
+		                    <img src="img/11.jpg" alt="">
+		                  </a>
+		                  <div class="content-text"></div>
+		                </div>
+		                <div class="min-circle" data-inside="min-circle">
+		                  <a href="img/13.jpg">
+		                    <img src="img/13.jpg" alt="">
+		                  </a>
+		                  <div class="content-text"></div>
+
+		                </div>
+		                <div class="min-circle" data-inside="min-circle">
+		                  <a href="img/14.jpg">
+		                    <img src="img/14.jpg" alt="">
+		                  </a>
+		                  <div class="content-text"></div>
+		                </div>
+		                <div class="min-circle" data-inside="min-circle">
+		                  <a href="img/17.jpg">
+		                    <img src="img/17.jpg" alt="">
+		                  </a>
+		                  <div class="content-text"></div>
+		                </div>
+		                <div class="min-circle" data-inside="min-circle">
+		                  <a href="img/19.jpg">
+		                    <img src="img/19.jpg" alt="">
+		                  </a>
+		                  <div class="content-text"></div>
+		                </div>
+		                <div class="min-circle" data-inside="min-circle">
+		                  <a href="img/2.jpg">
+		                    <img src="img/2.jpg" alt="">
+		                  </a>
+		                  <div class="content-text"></div>
+		                </div>
+		                
+		              </div>
+		            </div>
+		            </div>
+		        </div>
+
+
+
+
+			
+			<div class="content">
+					
+
+				<div class="grid">
+					
+			<h2 id="学习心路" style="color: #000000;"><img src="img/左.png">学习心路<img src="img/右.png"></h2>
+					<figure class="effect-julia">
+						<img src="img/1.jpg" alt="img1"/>
+						<figcaption>
+							<h2>焦伟康<span>171033</span></h2>
+							<div>
+								<p>Julia dances in the deep dark</p>
+								<p>She loves the smell of the ocean</p>
+								<p>And dives into the morning light</p>
+							</div>
+							<a href="http://www.htmleaf.com/">View more</a>
+						</figcaption>			
+					</figure>
+					<figure class="effect-julia">
+						<img src="img/10.jpg" alt="img10"/>
+						<figcaption>
+							<h2>郑洁 <span>17112229</span></h2>
+							<div>
+								<p>输不丢人，怕才丢人</p>
+								<p>很多事情不是你做不到，而是你不敢开始</p>
+								<p>勇敢走出第一步吧！</p>
+							</div>
+							<a href="http://www.htmleaf.com/">View more</a>
+						</figcaption>			
+					</figure>
+				</div>
+				
+				<div class="grid">
+					<figure class="effect-goliath">
+						<img src="img/11.jpg" alt="img11"/>
+						<figcaption>
+							<h2>李澍 <span>16104108</span></h2>
+							<p>When Goliath comes out, you should run.</p>
+							<a href="http://www.htmleaf.com/">View more</a>
+						</figcaption>			
+					</figure>
+					<figure class="effect-goliath">
+						<img src="img/13.jpg" alt="img13"/>
+						<figcaption>
+							<h2>于思源 <span>17103320</span></h2>
+							<p>When Goliath comes out, you should run.</p>
+							<a href="http://www.htmleaf.com/">View more</a>
+						</figcaption>			
+					</figure>
+				</div>
+				
+				<div class="grid">
+					<figure class="effect-hera">
+						<img src="img/14.jpg" alt="img14"/>
+						<figcaption>
+							<h2>潘帅<span>15102310</span></h2>
+							<p>
+								<a href="http://www.htmleaf.com/"><i class="fa fa-fw fa-file-pdf-o"></i></a>
+								<a href="http://www.htmleaf.com/"><i class="fa fa-fw fa-file-image-o"></i></a>
+								<a href="http://www.htmleaf.com/"><i class="fa fa-fw fa-file-archive-o"></i></a>
+								<a href="http://www.htmleaf.com/"><i class="fa fa-fw fa-file-code-o"></i></a>
+							</p>
+						</figcaption>			
+					</figure>
+					<figure class="effect-hera">
+						<img src="img/17.jpg" alt="img17"/>
+						<figcaption>
+							<h2>胡林涛 <span>17026204</span></h2>
+							<p>
+								<a href="http://www.htmleaf.com/"><i class="fa fa-fw fa-file-pdf-o"></i></a>
+								<a href="http://www.htmleaf.com/"><i class="fa fa-fw fa-file-image-o"></i></a>
+								<a href="http://www.htmleaf.com/"><i class="fa fa-fw fa-file-archive-o"></i></a>
+								<a href="http://www.htmleaf.com/"><i class="fa fa-fw fa-file-code-o"></i></a>
+							</p>
+						</figcaption>			
+					</figure>
+				</div>
+				
+				<div class="grid">
+					<figure class="effect-winston">
+						<img src="img/19.jpg" alt="img19"/>
+						<figcaption>
+							<h2>刘伟成 <span>17101409</span></h2>
+							<p>
+								<a href="http://www.htmleaf.com/"><i class="fa fa-fw fa-star-o"></i></a>
+								<a href="http://www.htmleaf.com/"><i class="fa fa-fw fa-comments-o"></i></a>
+								<a href="http://www.htmleaf.com/"><i class="fa fa-fw fa-envelope-o"></i></a>
+							</p>
+						</figcaption>			
+					</figure>
+					<figure class="effect-winston">
+						<img src="img/21.jpg" alt="img21"/>
+						<figcaption>
+							<h2>王焕顺 <span>17104215</span></h2>
+							<p>
+								<a href="http://www.htmleaf.com/"><i class="fa fa-fw fa-star-o"></i></a>
+								<a href="http://www.htmleaf.com/"><i class="fa fa-fw fa-comments-o"></i></a>
+								<a href="http://www.htmleaf.com/"><i class="fa fa-fw fa-envelope-o"></i></a>
+							</p>
+						</figcaption>			
+					</figure>
+				</div>
+				
+				<div class="grid">
+					<figure class="effect-selena">
+						<img src="img/22.jpg" alt="img22"/>
+						<figcaption>
+							<h2>胡晓野 <span>17103107</span></h2>
+							<p>Selena is a tiny-winged bird.</p>
+							<a href="http://www.htmleaf.com/">View more</a>
+						</figcaption>			
+					</figure>
+					<figure class="effect-selena">
+						<img src="img/24.jpg" alt="img24"/>
+						<figcaption>
+							<h2>胡文帅 <span>17104402</span></h2>
+							<p>Selena is a tiny-winged bird.</p>
+							<a href="http://www.htmleaf.com/">View more</a>
+						</figcaption>			
+					</figure>
+				</div>
+				
+					
+				
+			</div>
+			
+		</div>
+	
+	
+
+<div class="grid">
+	<h2 id="学习方法" style="color: #000000;"><img src="img/右.png">学习方法<img src="img/左.png"></h2>
+					<figure class="effect-zoe">
+						<img src="img/26.jpg" alt="img26"/>
+						<figcaption>
+						
+							<h3>参考网址</h3>
+							<div style="float: left;"></h3><h4>文档：</h4><h3><a href="http://www.runoob.com/" style="text-decoration: none;color: #FFFFFF;">菜鸟教程</a><br><br><br>
+							<a href="http://www.w3school.com.cn/" style="text-decoration: none;color: #FFFFFF;">W3SChool</a> </h3></div>
+							<div style="float: right;"><h4>视频：</h4>
+								<h3>
+							<a href="https://www.imooc.com/"style="text-decoration: none;color: #FFFFFF;">慕课网</a><br><br><br>
+							<a href="http://study.163.com/"	style="text-decoration: none;color: #FFFFFF;">网易云课堂</a>	</h3></div>
+
+						</figcaption>			
+					</figure>
+					<figure class="effect-zoe">
+						<img src="img/27.jpg" alt="img27"/>
+						<figcaption>
+							<h2>HTML</h2>
+							<div style="height: 50%; text-decoration: none;color: #FFFFFF;">
+						<div style="float: left"><h5>
+						<a href="http://www.w3school.com.cn/html/html_headings.asp"style="text-decoration: none;color: #FFFFFF;">HTML标题</a>	<br><br>
+							<a href="http://www.w3school.com.cn/html/html_paragraphs.asp"style="text-decoration: none;color: #FFFFFF;">HTML段落</a><br><br>
+							<a href="http://www.w3school.com.cn/html/html_css.asp"style="text-decoration: none;color: #FFFFFF;">HTML css</a><br><br>
+							<a href="http://www.w3school.com.cn/html/html_comments.asp"style="text-decoration: none;color: #FFFFFF;">HTML注释</a><br><br>
+						<a href="http://www.w3school.com.cn/html/html_links.asp"style="text-decoration: none;color: #FFFFFF;">HTML链接</a><br><br>
+						</h5></div>
+						<div style="float: right;"><br><br> <h3>HTML是最基础的！！！</h3></div>
+							</div>
+						<div style="height: 50%;">
+						<div style="float: right;"><h5>
+						<a href="http://www.w3school.com.cn/html/html_images.asp"style="text-decoration: none;color: #FFFFFF;">HTML图像</a><br><br>
+				<a href="http://www.w3school.com.cn/html/html_blocks.asp"style="text-decoration: none;color: #FFFFFF;">HTML块</a><br><br>
+				<a href="http://www.w3school.com.cn/html/html_classes.asp"style="text-decoration: none;color: #FFFFFF;">	HTML类</a><br><br>
+					<a href="http://www.w3school.com.cn/html/html_iframe.asp"	style="text-decoration: none;color: #FFFFFF;">HTML内联框架</a><br><br>
+					<a href="http://www.w3school.com.cn/html/html_backgrounds.asp"style="text-decoration: none;color: #FFFFFF;">HTML背景</a><br><br>
+					<a href="http://www.w3school.com.cn/html/html_scripts.asp"style="text-decoration: none;color: #FFFFFF;" >HTML脚本</a>	<br><br>
+					</h5></div></div>
+						</figcaption>			
+					</figure>
+				</div>
+		<div class="grid">
+					<figure class="effect-zoe">
+						<img src="img/28.jpg" alt="img28"/>
+						<figcaption>
+							<h2>CSS</h2>
+							<div style="height: 50%;">
+							<div style="float: right;"><h5>
+							<a href="http://www.w3school.com.cn/css/css_syntax_id_selector.asp"style="text-decoration: none;color: #FFFFFF;"	>CSS id选择器</a><br><br>
+							<a href="http://www.w3school.com.cn/css/css_syntax_class_selector.asp"style="text-decoration: none;color: #FFFFFF;">CSS类选择器</a><br><br>
+							
+						<a href="http://www.w3school.com.cn/css/css_background.asp" style="text-decoration: none;color: #FFFFFF;">CSS背景</a>	<br><br>
+					<a href="http://www.w3school.com.cn/css/css_text.asp"style="text-decoration: none;color: #FFFFFF;"	>CSS文本</a>	<br><br>
+					</h5></div><div style="float: left;">
+						<h3>CSS一种样式！！！</h3>
+					</div>
+							</div>
+					<div style="height: 50%;">
+							<div  style="float: left"><h5>
+									<a href="http://www.w3school.com.cn/css/css_font.asp"style="text-decoration: none;color: #FFFFFF;">	CSS字体</a>	<br><br>
+					
+						<a href="http://www.w3school.com.cn/css/css_link.asp"  style="text-decoration: none;color: #FFFFFF;">CSS链接</a><br>	<br>
+					<a href="http://www.w3school.com.cn/css/css_list.asp" style="text-decoration: none;color: #FFFFFF;">CSS列表</a><br>  <br>
+					<a href="http://www.w3school.com.cn/css/css_table.asp"style="text-decoration: none;color: #FFFFFF;"	>CSS表格</a>	<br><br>
+					<a href="http://www.w3school.com.cn/css/css_boxmodel.asp"style="text-decoration: none;color: #FFFFFF;"	>CSS框模型</a><br>	<br>
+					<a href="http://www.w3school.com.cn/css/css_positioning.asp"style="text-decoration: none;color: #FFFFFF;"	>CSS定位</a>	<br><br>
+					</h5></div></div>
+							
+						</figcaption>			
+					</figure>
+					<figure class="effect-zoe">
+						<img src="img/30.jpg" alt="img/30.jpg"/>
+						<figcaption>
+							<h2>Javascript</h2>
+							<div style="float: left;"><h5>框架引用：bootshrap</h5><h4>Javascript:从头开始学吧！！！</h4></div>
+							<br><br>
+					<div style="float: right;"	>	<h4>Jquery:</h4>运用框架jquery之家！<h4>Vue.jsNode.js(后端）</h4></div>
+						</figcaption>			
+					</figure>
+				</div>
+
+
+
+
+
+
+
+
+
+	
+	<div class="grid">
+		<h2 style="color: #000000;" id="学习成果"><img src="img/右.png">学习成果<img src="img/左.png"></h2>
+					
+					<figure class="effect-apollo">
+						<img src="img/4.jpg" alt="img4"/>
+						<figcaption>
+							<h2>第一节课 <span>作业</span></h2>
+							<p>个性化问卷调查</p>
+							<a href="http://www.htmleaf.com/">问卷HTML</a>
+						</figcaption>			
+					</figure>
+					<figure class="effect-apollo">
+						<img src="img/5.jpg" alt="img5"/>
+						<figcaption>
+							<h2>第二节课 <span>作业</span></h2>
+							<p>超炫的网页简历</p>
+							<a href="http://www.htmleaf.com/">HTML+CSS</a>
+						</figcaption>			
+					</figure>
+				</div>
+</div>
+<script src="js/jCircle.min.js"></script>
+	<script>
+	      var circle=new jCircle({
+	    'container': 'circles-container',
+	    'circle': 'circle',
+	    'mainContent':'main-circle-content',
+	    'animateCircles':true,
+	    'speed':3,
+	    'mainViewStyle':'normal',
+	    'minCirclesEffectOver':'pulse',
+	    'contentType':'images',
+	    'stopOnOverMain':false,
+	    'mainContentOverAction':'normal'
+	  });
+	  circle.create();
+	  
+	  var textCircle=new jCircle({
+	    'container': 'circles-container-text',
+	    'circle': 'circle-text',
+	    'mainContent':'main-circle-content-text',
+	    'animateCircles':true,
+	    'speed':3,
+	    'mainViewStyle':'normal',
+	    'minCirclesEffectOver':'rotate',
+	    'contentType':'text',
+	    'stopOnOverMain':true
+	  });
+	  textCircle.create();
+	    </script>
+
+</body>
+</html>
